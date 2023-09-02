f0aa948449526c2b0cab3a4832607e0594e3e63f
DmitriyAgapov <prweb2012@gmail.com>
2023-09-02 06:51:49 +0500

    init

---
+++ back/config/plugins.ts
@@ -0,0 +1,23 @@
+module.exports = ({ env }) => ({
+	'duplicate-button': true,
+	seo: {
+		enabled: true,
+	},
+	graphql: {
+		enabled: true,
+		config: {
+			endpoint: '/graphql',
+			shadowCRUD: true,
+			playgroundAlways: false,
+			depthLimit: 10,
+			defaultLimit: 100,
+			amountLimit: 100,
+			maxLimit: 150,
+
+			apolloServer: {
+
+				tracing: false,
+			},
+		},
+	},
+});
